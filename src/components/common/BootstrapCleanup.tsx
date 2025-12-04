b12fd07c6b5d97161c77dbee61ac2642c4325edd
yasinkhan2908 <khan.yasin97@gmail.com>
2025-12-04 12:55:00 +0530

    push detail  page

--- src/components/common/BootstrapCleanup.tsx
+++ src/components/common/BootstrapCleanup.tsx
@@ -6,18 +6,24 @@ export default function BootstrapCleanup() {
   const pathname = usePathname();
 
   const removeBootstrapStuff = () => {
+    // Remove Bootstrap modal class
     document.body.classList.remove("modal-open");
 
+    // Remove inline styles that block scroll
+    document.body.style.overflow = "";
+    document.body.style.paddingRight = "";
+
+    // Remove all modal backdrops
     const backdrops = document.querySelectorAll(".modal-backdrop");
     backdrops.forEach((b) => b.remove());
   };
 
-  // Cleanup when route changes
+  // Run cleanup when route changes
   useEffect(() => {
     removeBootstrapStuff();
   }, [pathname]);
 
-  // Cleanup when component unmounts
+  // Also run cleanup on unmount
   useEffect(() => {
     return () => removeBootstrapStuff();
   }, []);
