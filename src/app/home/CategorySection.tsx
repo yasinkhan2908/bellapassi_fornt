b8db211b170761a81e1f014100f4e6f557191203
yasinkhan2908 <khan.yasin97@gmail.com>
2025-12-15 13:40:44 +0530

    push code

--- src/app/home/CategorySection.tsx
+++ src/app/home/CategorySection.tsx
@@ -7,22 +7,20 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import Link from "next/link";
-
 export const CategorySection = ({ categories }: CategorySectionProps) => {
   return (
     <section className="category-section" data-aos="fade-up">
       <div className="container">
+
         <div className="max-w-3xl mx-auto my-8 mt-4">
           <div className="d-flex flat-title flex-row justify-content-between align-items-center px-0 wow fadeInUp mb-5">
-            <h3 className="title">Season Collection</h3>
-            <Link href="shop-collection-sub.html" className="tf-btn btn-line" prefetch={false}>
-              View all<i className="bi bi-arrow-right pl-2"></i>
-            </Link>
+              <h3 className="title">Season Collection</h3>
+              <Link href="shop-collection-sub.html" className="tf-btn btn-line" prefetch={false}>View all<i className="bi bi-arrow-right pl-2"></i></Link>
           </div>
           <Swiper
             breakpoints={{
               0: { slidesPerView: 2 },
-              480: { slidesPerView: 2 },
+              480: { slidesPerView: 2},
               640: { slidesPerView: 3 },
               768: { slidesPerView: 3 },
               1024: { slidesPerView: 6 },
@@ -34,21 +32,12 @@ export const CategorySection = ({ categories }: CategorySectionProps) => {
             pagination={{ clickable: true }}
             navigation
           >
-            {categories.map((category) => (
-              <SwiperSlide key={category.id} className="text-center">
+            {categories.map((category, index) => (
+              <SwiperSlide  key={category.id} className="text-center">
                 <Link href={category.seo} prefetch={false}>
-                  <div className="relative aspect-square w-full max-w-[84px] mx-auto">
-                    <Image
-                      src={category.image}
-                      alt={category.name}
-                      fill
-                      sizes="(max-width: 640px) 84px, (max-width: 768px) 84px, (max-width: 1024px) 84px, 84px"
-                      className="object-cover img-fluid rounded-full"
-                      quality={75}
-                      loading="lazy"
-                    />
-                  </div>
+                  <Image src={category.image} height={175} width={175} alt="Slide 1" loading="lazy" />
                   <div className="text-center category-title">{category.name}</div>
+                  {/* <div className="text-center category-subtitle">0 items</div> */}
                 </Link>
               </SwiperSlide>
             ))}
